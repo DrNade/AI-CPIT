e09ec78d7a4b31b13f3b9f4b7a76adf7e18d3eac
DrNade <sahilomar@gmail.com>
2026-02-12 23:49:17 +0500

    1st assignment if statement

--- index.py
+++ index.py
@@ -7,10 +7,36 @@
 # print (nadeem)
 # num = input ("Enter the name of the user: ")
 # print (num)
-n = "68"
-print (n, type(n))
+# n = "68"
+# print (n, type(n))
 
+# Required minimum values
+req_education = 12
+req_height = 5.7
+req_age = 18
 
+# User input
+education = float(input("Enter education (in years): "))
+height = float(input("Enter height (in feet): "))
+age = int(input("Enter age: "))
+
+# Check conditions
+conditions_met = 0
+
+if education >= req_education:
+    conditions_met += 1
+
+elif height >= req_height:
+    conditions_met += 1
+
+if age >= req_age:
+    conditions_met += 1
+
+# Final decision
+if conditions_met >= 2:
+    print("ok")
+else:
+    print("not good")
 
 
 
