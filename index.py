0a5429c41a7e582534bc02294c9f316bb8189343
DrNade <sahilomar@gmail.com>
2026-02-12 23:18:37 +0500

    first commit

---
+++ index.py
@@ -0,0 +1,17 @@
+# print ("Janab")
+# print ("34")
+# print ("34.5")
+# name = 99
+# nadeem = "nadeem aslam"
+# print (name)
+# print (nadeem)
+# num = input ("Enter the name of the user: ")
+# print (num)
+n = "68"
+print (n, type(n))
+
+
+
+
+
+
