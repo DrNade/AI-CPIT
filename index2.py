301804e51798fad335bc7b2ec7a1bea2e0cd1616
DrNade <sahilomar@gmail.com>
2026-02-14 12:05:16 +0500

    2nd loop table assignment if statement

---
+++ index2.py
@@ -0,0 +1,146 @@
+# print("hello world")
+# print(34)
+# name = 99
+# print(name)
+
+# FirstName # Pascal case writinge variable
+# firstName # camel case
+# first_name # snake case
+# primeMinister1st 
+
+# first$name = "hello"
+# print(first$name)
+# 1stname 
+
+# number = (12 + 5) * 2
+# print(number)
+
+# num1 = 5
+# num2 = 10
+# print(num1 * num2 + 10)
+
+# num = input("enter the name : ")
+# print(num)
+
+# n = "45"
+# print(n + type(n))
+
+# name = "nabeel"
+# print(name)
+
+# number = input("enter your number: ")
+# print(number, type(number))
+# number = int(number)
+# print(number, type(number))
+
+# number = int(input("Enter your number: "))
+# print(number, type(number))
+
+# number = float(input("Enter your number: "))
+# print(number, type(number))
+
+# number = 44
+# if number=="44":
+#     print("yes you are right")
+# else:
+#     print("Oh! no you are wrong")
+
+# marks = 99
+# if marks == 90:
+#     print("yes you are right")
+# elif marks == 99:
+#     print("hurrey! you got it")
+# else:
+#     print("Oh! no you are wrong")
+
+
+# marks = 980
+# if marks > 40:
+#     print("failed...")
+# elif marks > 100:
+#     print("your marks are not acceptable")
+# elif marks > 90:
+#     print("pass ho gaya: A+ Grade")
+# elif marks > 80:
+#     print("B+ Grade")
+# elif marks > 70:
+#     print("C+ Grade")
+# elif marks > 60:
+#     print("D+ Grade")
+# elif marks > 50:
+#     print("E+ Grade")
+# else:
+#     print("failed")
+
+# marks = 990
+# if marks > 90 and marks < 101:
+#     print("A+ Grade pass")
+# else:
+#     print("Failed...")
+
+# a = 18
+# e = 12
+# h= 5.6
+
+# if a > 17 and e > 11:
+#     print("pass")
+# elif e > 11 and h > 5.6:
+#     print("Pass")
+# elif h > 5.6 and a > 17:
+#     print("Pass")
+# else:
+#     print("Failed")
+
+# if (a > 17 and e > 11) or (e > 11 and h > 5.6) or (h > 5.6 and a > 17):
+#     print("Pass")
+# else:
+#     print("Failed")
+
+# num = 4
+
+# match(num):
+#     case 0:
+#         print("value 0")
+#     case 1:
+#         print("value 1")
+#     case 12:
+#         print("yes value is 12")
+#     case _:
+#         print("Oh no")
+
+# i= 0
+# while i < 10:
+#     print(i, " Pakistan")
+#     i+=1
+
+# for i in range(2,10, 2):
+#     print(i)
+
+a = 5
+b = 10
+# print(a,b)
+# [a,b] = [b,a]
+# print(a,b)
+
+# a =a + b  # 5 + 10 = 15;
+# b =a - b  # 15 - 10 = 5
+# a =a - b  # 15 - 5 = 10
+
+# a =a ^ b  # 5 + 10 = 15;
+# b =a ^ b  # 15 - 10 = 5
+# a =a ^ b  # 15 - 5 = 10
+
+# temp = a 
+# a = b 
+# b = temp
+# print(a,b)
+
+for i in range(0,50):
+    # print(f" {i} hello world")
+    print( str(i) + " hello world")
+    if i == 39:
+        continue
+
+
+
+
